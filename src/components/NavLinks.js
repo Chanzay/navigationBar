7cd356f6ff9408949b774a34d37141682ed7bfdc
Dakota Chanthakoummane <chanzay.c@gmail.com>
2024-10-04 18:13:52 -0400

    second commit

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavLinks({mobileTab}) {
   return (
-    <div className={`linkTab ${mobileTab == true ? 'linkTabActive': ''}`}>
+    <div className={`linkTab ${mobileTab === true ? 'linkTabActive': ''}`}>
       <a href='/#' className='tab'>Home</a>
       <a href='/#' className='tab'>Shop</a>
       <a href='/#' className='tab'>Search</a>
